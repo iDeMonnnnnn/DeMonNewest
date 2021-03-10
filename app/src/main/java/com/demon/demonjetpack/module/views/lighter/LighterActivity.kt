82366be6346078dc7bd083b9ec65f3f8de782cb4
DeMon <757454343@qq.com>
2021-03-10 17:30:07 +0800

    工厂模式:ExoPlayer

--- app/src/main/java/com/demon/demonjetpack/module/views/lighter/LighterActivity.kt
+++ app/src/main/java/com/demon/demonjetpack/module/views/lighter/LighterActivity.kt
@@ -1,16 +1,23 @@
 package com.demon.demonjetpack.module.views.lighter
 
 import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.demon.basemvvm.mvvm.BaseViewModel
 import com.demon.basemvvm.mvvm.MvvmActivity
 import com.demon.demonjetpack.R
 import com.demon.demonjetpack.databinding.ActivityLighterBinding
-import com.demon.demonjetpack.databinding.LighterTipCommonBinding
+import com.demon.demonjetpack.databinding.LighterTip1Binding
+import com.demon.demonjetpack.databinding.LighterTip2Binding
+import com.demon.demonjetpack.databinding.LighterTip3Binding
 import me.samlss.lighter.Lighter
 import me.samlss.lighter.parameter.Direction
 import me.samlss.lighter.parameter.LighterParameter
 import me.samlss.lighter.parameter.MarginOffset
+import me.samlss.lighter.shape.CircleShape
 import me.samlss.lighter.shape.OvalShape
+import me.samlss.lighter.shape.RectShape
 
 class LighterActivity : MvvmActivity<ActivityLighterBinding, BaseViewModel>() {
 
@@ -32,19 +39,19 @@ class LighterActivity : MvvmActivity<ActivityLighterBinding, BaseViewModel>() {
             .addHighlight(
                 LighterParameter.Builder()
                     .setHighlightedViewId(R.id.btn1)
-                    .setTipView(createCommonTipView("解决烦恼，第一步", "下一步"))
-                    .setLighterShape(OvalShape())
-                    .setTipViewRelativeDirection(Direction.RIGHT)
-                    .setTipViewRelativeOffset(MarginOffset(100, 0, 0, 0))
+                    .setTipView(createCommonTipView1())
+                    .setLighterShape(CircleShape())
+                    .setTipViewRelativeDirection(Direction.TOP)
+                    .setTipViewRelativeOffset(MarginOffset(0, 0, 0, -100))
                     .build()
             )
             .addHighlight(
                 LighterParameter.Builder()
                     .setHighlightedViewId(R.id.btn2)
-                    .setTipView(createCommonTipView("专业服务，第二步", "下一步"))
-                    .setLighterShape(OvalShape())
-                    .setTipViewRelativeDirection(Direction.RIGHT)
-                    .setTipViewRelativeOffset(MarginOffset(50, 0, 100, 0))
+                    .setTipView(createCommonTipView2())
+                    .setLighterShape(RectShape())
+                    .setTipViewRelativeDirection(Direction.BOTTOM)
+                    .setTipViewRelativeOffset(MarginOffset(0, 0, 0, 0))
                     .build(),
                 LighterParameter.Builder()
                     .setHighlightedViewId(R.id.btn3)
@@ -54,19 +61,46 @@ class LighterActivity : MvvmActivity<ActivityLighterBinding, BaseViewModel>() {
             ).addHighlight(
                 LighterParameter.Builder()
                     .setHighlightedViewId(R.id.btn4)
-                    .setTipView(createCommonTipView("分享直播间，第三步", "知道了"))
-                    .setLighterShape(OvalShape())
-                    .setTipViewRelativeDirection(Direction.BOTTOM)
-                    .setTipViewRelativeOffset(MarginOffset(0, 150, 100, 0))
+                    .setTipView(createCommonTipView3())
+                    .setLighterShape(RectShape())
+                    .setTipViewRelativeDirection(Direction.TOP)
+                    .setTipViewRelativeOffset(MarginOffset(0, 150, 0, 0))
                     .build()
             )
     }
 
+    fun createCommonTipView1(): View {
+        val binding = LighterTip1Binding.inflate(layoutInflater)
+        val optionsScaleType = RequestOptions().override(SIZE_ORIGINAL)
+        Glide.with(this).load(R.drawable.gif1)
+            .apply(optionsScaleType)
+            .into(binding.ivClick)
+        binding.ivButton.setOnClickListener {
+            lighter.next()
+        }
+        return binding.root
+    }
 
-    fun createCommonTipView(string1: String?, string2: String?): View {
-        val binding = LighterTipCommonBinding.inflate(layoutInflater)
-        binding.tvText.text = string1
-        binding.ivButton.text = string2
+
+    fun createCommonTipView2(): View {
+        val binding = LighterTip2Binding.inflate(layoutInflater)
+        val optionsScaleType = RequestOptions().override(SIZE_ORIGINAL)
+        Glide.with(this).load(R.drawable.gif2)
+            .apply(optionsScaleType)
+            .into(binding.ivClick)
+        binding.ivButton.setOnClickListener {
+            lighter.next()
+        }
+        return binding.root
+    }
+
+
+    fun createCommonTipView3(): View {
+        val binding = LighterTip3Binding.inflate(layoutInflater)
+        val optionsScaleType = RequestOptions().override(SIZE_ORIGINAL)
+        Glide.with(this).load(R.drawable.gif3)
+            .apply(optionsScaleType)
+            .into(binding.ivClick)
         binding.ivButton.setOnClickListener {
             lighter.next()
         }
