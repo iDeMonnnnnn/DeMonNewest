d37aeadf92778ba4b78b169714c91d8654655afd
DeMon <757454343@qq.com>
2022-03-08 11:44:52 +0800

    系统ActivityResultContract

--- app/src/main/java/com/demon/demonjetpack/module/ara/ActResultActivity.kt
+++ app/src/main/java/com/demon/demonjetpack/module/ara/ActResultActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Environment
 import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.net.toUri
 import com.demon.basemvvm.intent.DeMonActivityResult
 import com.demon.basemvvm.intent.forActivityResult
 import com.demon.basemvvm.intent.pairIntent
@@ -15,6 +16,7 @@ import com.demon.demonjetpack.databinding.ActivityActResultBinding
 import com.demon.qfsolution.utils.MimeType
 import com.demon.qfsolution.utils.getExternalOrFilesDirPath
 import com.demon.qfsolution.utils.getFileUri
+import com.demon.qfsolution.utils.isFileExists
 import java.io.File
 
 class ActResultActivity : MvvmActivity<ActivityActResultBinding, BaseViewModel>() {
@@ -22,12 +24,16 @@ class ActResultActivity : MvvmActivity<ActivityActResultBinding, BaseViewModel>(
     private val requestPermission = DeMonActivityResult(this, ActivityResultContracts.RequestPermission())
     private val requestMultiplePermissions = DeMonActivityResult(this, ActivityResultContracts.RequestMultiplePermissions())
     private val takePicture = DeMonActivityResult(this, ActivityResultContracts.TakePicture())
+    private val takePicturePreview = DeMonActivityResult(this, ActivityResultContracts.TakePicturePreview())
     private val captureVideo = DeMonActivityResult(this, ActivityResultContracts.CaptureVideo())
     private val pickContact = DeMonActivityResult(this, ActivityResultContracts.PickContact())
     private val getContent = DeMonActivityResult(this, ActivityResultContracts.GetContent())
+    private val getMultipleContents = DeMonActivityResult(this, ActivityResultContracts.GetMultipleContents())
     private val openDocument = DeMonActivityResult(this, ActivityResultContracts.OpenDocument())
     private val openMultipleDocuments = DeMonActivityResult(this, ActivityResultContracts.OpenMultipleDocuments())
+    private val createDocument = DeMonActivityResult(this, ActivityResultContracts.CreateDocument())
     private val openDocumentTree = DeMonActivityResult(this, ActivityResultContracts.OpenDocumentTree())
+
     override fun initData() {
         setToolbar("Activity Result API")
 
@@ -70,22 +76,20 @@ class ActResultActivity : MvvmActivity<ActivityActResultBinding, BaseViewModel>(
             }
 
             btn4.setOnClickThrottleFirst {
-                val savePath = "${getExternalOrFilesDirPath(Environment.DIRECTORY_DCIM)}/${System.currentTimeMillis()}.mp4"
-                val uri = File(savePath).getFileUri()
-                captureVideo.launch(uri) {
-                    Log.i(TAG, "captureVideo: $it")
+                takePicturePreview.launch(null) {
+                    Log.i(TAG, "takePicturePreview: $it")
                 }
             }
 
             btn5.setOnClickThrottleFirst {
-                pickContact.launch(null) {
-                    Log.i(TAG, "pickContact: $it")
+                getContent.launch(MimeType.all) {
+                    Log.i(TAG, "getContent: $it  ${it.isFileExists()}")
                 }
             }
 
             btn6.setOnClickThrottleFirst {
-                getContent.launch(MimeType.img) {
-                    Log.i(TAG, "getContent: $it")
+                getMultipleContents.launch(MimeType.all) {
+                    Log.i(TAG, "getMultipleContents: $it")
                 }
             }
 
@@ -102,9 +106,29 @@ class ActResultActivity : MvvmActivity<ActivityActResultBinding, BaseViewModel>(
             }
 
             btn9.setOnClickThrottleFirst {
-                val uri = File(getExternalOrFilesDirPath(null)).getFileUri()
-                openDocumentTree.launch(uri){
+                val uri = getExternalOrFilesDirPath(null).toUri()
+                openDocumentTree.launch(uri) {
                     Log.i(TAG, "openDocumentTree: $it")
+                }
+            }
+
+            btn10.setOnClickThrottleFirst {
+                createDocument.launch("${System.currentTimeMillis()}.txt") {
+                    Log.i(TAG, "createDocument: $it")
+                }
+            }
+
+            btn11.setOnClickThrottleFirst {
+                val savePath = "${getExternalOrFilesDirPath(Environment.DIRECTORY_DCIM)}/${System.currentTimeMillis()}.mp4"
+                val uri = File(savePath).getFileUri()
+                captureVideo.launch(uri) {
+                    Log.i(TAG, "captureVideo: $it")
+                }
+            }
+
+            btn12.setOnClickThrottleFirst {
+                pickContact.launch(null) {
+                    Log.i(TAG, "pickContact: $it")
                 }
             }
 
