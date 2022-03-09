972beecae9da79cab9e12e97e93e393c51d08c10
DeMon <757454343@qq.com>
2022-03-09 14:53:26 +0800

    paging_version = "3.1.0"

--- app/src/main/java/com/demon/demonjetpack/module/paging/PagingActivity.kt
+++ app/src/main/java/com/demon/demonjetpack/module/paging/PagingActivity.kt
@@ -4,10 +4,10 @@ import androidx.lifecycle.lifecycleScope
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.demon.basemvvm.mvvm.MvvmActivity
 import com.demon.demonjetpack.base.data.RouterConst
+import com.demon.demonjetpack.base.list.DataViewHolder
 import com.demon.demonjetpack.bean.ArticleBean
 import com.demon.demonjetpack.databinding.ActivityPagingBinding
 import com.demon.demonjetpack.databinding.ListArticleBinding
-import com.demon.demonjetpack.base.list.DataViewHolder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -15,15 +15,19 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class PagingActivity : MvvmActivity<ActivityPagingBinding, PagingViewModel>() {
 
-    override fun initData() {
-        setToolbar("Paging3")
-        val pagingAdapter = object : PagingAdapter<ListArticleBinding, ArticleBean>(binding.swipe) {
+
+    private val pagingAdapter by lazy {
+        object : PagingAdapter<ListArticleBinding, ArticleBean>(binding.swipe) {
             override fun onBind(holder: DataViewHolder<ListArticleBinding>, position: Int, data: ArticleBean) {
                 holder.binding.run {
                     tvText.text = data.title
                 }
             }
         }
+    }
+
+    override fun initData() {
+        setToolbar("Paging3")
         binding.rv.adapter = pagingAdapter.withFooter()
 
         mViewModel.getAuthorList("鸿洋").observe(this@PagingActivity) {
