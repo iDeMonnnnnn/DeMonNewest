6f54e3134b7bd29a65790913fe0a6336a7140b0f
DeMon <757454343@qq.com>
2022-09-22 18:08:34 +0800

    fix:compose黑夜模式适配

--- app/src/main/java/com/demon/demonnewest/module/compose/ComposeActivity.kt
+++ app/src/main/java/com/demon/demonnewest/module/compose/ComposeActivity.kt
@@ -3,17 +3,29 @@ package com.demon.demonnewest.module.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.demon.demonnewest.R
 import com.demon.demonnewest.bean.MessageBean
@@ -22,15 +34,54 @@ import com.demon.demonnewest.module.compose.ui.theme.DeMonNewestTheme
 class ComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val list = List(100) {
+            MessageBean(
+                "Android-$it", "Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose"
+            )
+        }
         setContent {
             DeMonNewestTheme {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    MessageCard(MessageBean("Android", "Jetpack Compose"))
+                Surface() {
+                    Column() {
+                        TopAppBar(title = {
+                            Text(text = "Compose")
+                        })
+                        Conversation(msgs = list)
+                    }
                 }
             }
         }
     }
 
+
+    @Preview("Featured Post • Dark")
+    @Composable
+    fun pre() {
+        val list = List(100) {
+            MessageBean(
+                "Android-$it", "Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose,Jetpack Compose"
+            )
+        }
+        DeMonNewestTheme {
+            Surface() {
+                Column() {
+                    TopAppBar(title = {
+                        Text(text = "Compose")
+                    })
+                    Conversation(msgs = list)
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Conversation(msgs: List<MessageBean>) {
+        LazyColumn {
+            items(msgs) { msg ->
+                MessageCard(msg = msg)
+            }
+        }
+    }
 
     @Composable
     fun MessageCard(msg: MessageBean) {
@@ -44,17 +95,26 @@ class ComposeActivity : ComponentActivity() {
                     .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+            var isExpand by rememberSaveable {
+                mutableStateOf(false)
+            }
+            val surfaceColor by animateColorAsState(
+                if (isExpand) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+            )
+            Column(modifier = Modifier.clickable { isExpand = !isExpand }) {
                 Text(
-                    text = msg.author,
-                    color = MaterialTheme.colors.secondaryVariant,
-                    style = MaterialTheme.typography.subtitle2
+                    text = msg.author, color = MaterialTheme.colors.secondaryVariant, style = MaterialTheme.typography.subtitle2
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = msg.body,
-                    style = MaterialTheme.typography.body2
-                )
+                Surface(
+                    shape = MaterialTheme.shapes.medium, elevation = 1.dp, color = surfaceColor, modifier = Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                ) {
+                    Text(
+                        text = msg.body, modifier = Modifier.padding(all = 4.dp), maxLines = if (isExpand) Int.MAX_VALUE else 1, style = MaterialTheme.typography.body2
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/demon/demonnewest/module/compose/ui/theme/Color.kt
+++ app/src/main/java/com/demon/demonnewest/module/compose/ui/theme/Color.kt
@@ -2,15 +2,10 @@ package com.demon.demonnewest.module.compose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryDark = Color(0xff03A9F4)
+val primaryVariantDark = Color(0xff2196F3)
+val secondaryDark = Color(0xFF03DAC6)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-
-val colorPrimary = Color(0xFF03A9F4)
-val colorPrimaryDark = Color(0xFF2196F3)
-val colorAccent = Color(0xFF00BCD4)
+val primaryLight = Color(0xff03A9F4)
+val primaryVariantLight = Color(0xff2196F3)
+val secondaryLight = Color(0xff00BCD4)
